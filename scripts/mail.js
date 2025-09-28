7fc39e329d4d82d5dea22e3d9161b7a945e912de
Thibolognaize <thibault.feat@gmail.com>
2025-09-28 19:53:27 +0200

    Ajout Recaptcha

--- scripts/mail.js
+++ scripts/mail.js
@@ -1,19 +1,43 @@
-// Initialiser mailjs avec User ID
 emailjs.init('psOL5hCv1Vl0UqHjX');
 
-// Écouter la soumission du formulaire
 document.getElementById('contact-form').addEventListener('submit', function (event) {
     event.preventDefault();
 
-    // Envoyer l'email via mailjs
-    emailjs.send('service_eadydh6', 'template_zn8w9if', {
-        name: document.getElementById('name').value,
-        company: document.getElementById('company').value,
-        email: document.getElementById('email').value,
-        message: document.getElementById('message').value
-    })
-        .then(function (response) {
-            alert('Email send !');
-            alert('Error while sending the email');
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let message = document.getElementById('message').value;
+
+    if (name === '' || email === '' || message === '') {
+        alert('Veuillez remplir tous les champs.');
+        return;
+    }
+
+    if (!validateEmail(email)) {
+        alert('Veuillez saisir une adresse email valide.');
+        return;
+    }
+
+    grecaptcha.ready(function () {
+        grecaptcha.execute('6LdI_9crAAAAAHQMPwCmzCpHBPDMtg5rcHkTUo7t', { action: 'submit' }).then(function (token) {
+            // Envoyer l'email via mailjs avec le token reCAPTCHA
+            emailjs.send('service_eadydh6', 'template_zn8w9if', {
+                name: name,
+                email: email,
+                message: message,
+                'g-recaptcha-response': token
+            })
+                .then(function (response) {
+                    console.log('Email envoyé avec succès !', response);
+                    alert('Email envoyé avec succès !');
+                }, function (error) {
+                    console.error('Erreur lors de l\'envoi de l\'email :', error);
+                    alert('Erreur lors de l\'envoi de l\'email.');
+                });
         });
+    });
 });
+
+function validateEmail(email) {
+    const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return re.test(email);
+}
